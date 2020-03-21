804ca609529e94ee751369f9dcc59970b20d668f
Mohammad <mohammad_m69@yahoo.com>
2020-03-21 12:37:16 +0430

    fix online users problem

--- src/Traits/CanVisit.php
+++ src/Traits/CanVisit.php
@@ -8,46 +8,43 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait CanVisit
 {
-    /**
-     * Get all of the post's comments.
-     *
-     * @return mixed
-     */
-    public function visitLogs()
-    {
-        return $this->morphMany(Visit::class, 'user');
-    }
-
-    /**
-     * Retrieve online users
-     *
-     * @param $query
-     * @param int $seconds
-     * @return mixed
-     */
-    public function scopeOnline($query, $seconds = 180)
-    {
-        $time = now()->subSeconds($seconds);
-
-        return $query->whereHas('visitLogs', function ($query) use ($time) {
-            $query->whereDate('visits.created_at', '>=', $time);
-        });
-    }
-
-    /**
-     * check if user is online
-     *
-     * @param int $seconds
-     * @return bool
-     */
-    public function isOnline($seconds = 180)
-    {
-        $time = now()->subSeconds($seconds);
-
-        return $this->visitLogs()->whereHasMorph('user', [static::class], function ($query) use ($time) {
-            $query
-                ->where('user_id', $this->id)
-                ->whereDate('visits.created_at', '>=', $time);
-        })->count() > 0;
-    }
+	/**
+	 * Get all of the post's comments.
+	 * @return mixed
+	 */
+	public function visitLogs()
+	{
+		return $this->morphMany(Visit::class, 'user');
+	}
+	
+	/**
+	 * Retrieve online users
+	 * @param $query
+	 * @param int $seconds
+	 * @return mixed
+	 */
+	public function scopeOnline($query, $seconds = 180)
+	{
+		$time = now()->subSeconds($seconds);
+		
+		return $query->whereHas('visitLogs', function ($query) use ($time) {
+			$query->where('visits.created_at', '>=', $time->toDateTime());
+		});
+	}
+	
+	/**
+	 * check if user is online
+	 * @param int $seconds
+	 * @return bool
+	 */
+	public function isOnline($seconds = 180)
+	{
+		$time = now()->subSeconds($seconds);
+		
+		return $this->visitLogs()->whereHasMorph('user', [ static::class ], function ($query) use ($time) {
+				$query
+					->where('user_id', $this->id)
+					->where('visits.created_at', '>=', $time->toDateTime());
+			})->count() > 0;
+	}
 }
