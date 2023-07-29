219a0e957b7558120518c52a9341e962e6e24087
ravindra135 <reddyravindra947@gmail.com>
2023-07-29 14:32:02 +0530

    fixed the indentation to 1 tab (4 spaces)

--- src/Middlewares/LogVisits.php
+++ src/Middlewares/LogVisits.php
@@ -8,52 +8,52 @@ use Illuminate\Support\Facades\Auth;
 
 class LogVisits
 {
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure  $next
-   *
-   * @return mixed
-   */
-  public function handle($request, Closure $next)
-  {
-      if(session()->has('visit')) {
-          $visitData = session()->get('visit');
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(session()->has('visit')) {
+            $visitData = session()->get('visit');
 
-          if($visitData['ip'] != $request->ip() || $visitData['url'] != $request->url()) {
-              $this->logVisit($request);
-          }
-      } else {
-          $this->logVisit($request);
-      }
+            if($visitData['ip'] != $request->ip() || $visitData['url'] != $request->url()) {
+                $this->logVisit($request);
+            }
+        } else {
+            $this->logVisit($request);
+        }
 
-      return $next($request);
-  }
+        return $next($request);
+    }
 
-  public function logVisit($request) {
+    public function logVisit($request) {
 
-      $logHasSaved = false;
+        $logHasSaved = false;
 
-      session(['visit' => [
-          'ip' => $request->ip(),
-          'url' => $request->url(),
-      ]]);
+        session(['visit' => [
+            'ip' => $request->ip(),
+            'url' => $request->url(),
+        ]]);
 
-      // create log for first binded model
-      foreach ($request->route()->parameters() as $parameter) {
-          if ($parameter instanceof Model) {
-              visitor()->visit($parameter);
+        // create log for first binded model
+        foreach ($request->route()->parameters() as $parameter) {
+            if ($parameter instanceof Model) {
+                visitor()->visit($parameter);
 
-              $logHasSaved = true;
+                $logHasSaved = true;
 
-              break;
-          }
-      }
+                break;
+            }
+        }
 
-      // create log for normal visits
-      if (!$logHasSaved) {
-          visitor()->visit();
-      }
-  }
+        // create log for normal visits
+        if (!$logHasSaved) {
+            visitor()->visit();
+        }
+    }
 }
