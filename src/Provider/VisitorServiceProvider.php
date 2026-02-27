cce639ff1030ee27d8ce966f4ff46519e76b2ec6
Abbas Arif <43321373+buzzclue@users.noreply.github.com>
2026-02-28 00:17:47 +0500

    Why empty stub?
    Removed commented-out code for adding geo columns migration and ensured timestamp variable is defined before usage.

--- src/Provider/VisitorServiceProvider.php
+++ src/Provider/VisitorServiceProvider.php
@@ -25,20 +25,20 @@ class VisitorServiceProvider extends ServiceProvider
             'config'
         );
 
+		$timestamp = date('Y_m_d_His', time());
         if (! class_exists('CreateVisitsTable')) {
-            $timestamp = date('Y_m_d_His', time());
-
             $this->publishes([
                 __DIR__ . '/../../database/migrations/create_visits_table.php.stub' => database_path("/migrations/{$timestamp}_create_visits_table.php"),
             ], 'migrations');
         }
 
-		if (! class_exists('AddGeoColumnsToVisitsTable')) {
-			$this->publishes([
-				__DIR__.'/../../database/migrations/add_geo_columns_to_visits_table.php.stub'
-					=> database_path("migrations/{$timestamp}_add_geo_columns_to_visits_table.php"),
-			], 'migrations');
-		}
+		// add_geo_columns_to_visits_table is empty also $timestamp is undefined because of above condition.
+		// if (! class_exists('AddGeoColumnsToVisitsTable')) {
+		// 	$this->publishes([
+		// 		__DIR__.'/../../database/migrations/add_geo_columns_to_visits_table.php.stub'
+		// 			=> database_path("migrations/{$timestamp}_add_geo_columns_to_visits_table.php"),
+		// 	], 'migrations');
+		// }
 
         $this->registerMacroHelpers();
     }
